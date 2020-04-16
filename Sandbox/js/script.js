c850e64960262eab75b2ba1e4688e54016375ebc
chad_turner <guyt2000@hotmail.com>
2020-04-16 11:02:47 -0500

    Module 4 ready for review

---
+++ Sandbox/js/script.js
@@ -0,0 +1,13 @@
+ (function(window) {
+ 
+  var obj = {};
+
+  obj.dreamOn = function () {
+   console.log("I want to see the global scope! Let me out!");
+ };
+
+ window.doer = obj;
+ 
+ })(window);
+
+doer.dreamOn();
