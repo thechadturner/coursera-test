980f48dab254d566782c43e59b48fcf37dbdba67
chad_turner <guyt2000@hotmail.com>
2021-02-23 11:23:22 +1300

    First try at adding php...this example only adds two numbers...

---
+++ Okta/js/scripts.js
@@ -0,0 +1,76 @@
+var oktaSignIn = new OktaSignIn({
+  baseUrl: "https://dev-49934482.okta.com",
+  clientId: "0oa7yhklz0BvikmFq5d6",
+  authParams: {
+    issuer: "default",
+    responseType: ['token', 'id_token'],
+    display: 'page'
+  }
+});
+
+if (oktaSignIn.token.hasTokensInUrl()) {
+  oktaSignIn.token.parseTokensFromUrl(
+    // If we get here, the user just logged in.
+    function success(res) {
+      var accessToken = res[0];
+      var idToken = res[1];
+
+      var uid = idToken.userId
+      
+      oktaSignIn.tokenManager.add('accessToken', accessToken);
+      oktaSignIn.tokenManager.add('idToken', idToken);
+
+      window.location.hash='';
+      document.getElementById("messageBox").innerHTML = "Hello, " + idToken.claims.email + "! You just logged in! :)";
+    },
+    function error(err) {
+      console.error(err);
+    }
+  );
+} 
+else 
+{
+  oktaSignIn.session.get(function (res) {
+    // If we get here, the user is already signed in.
+    if (res.status === 'ACTIVE') {
+      var uid = res.userId
+
+      document.getElementById("messageBox").innerHTML = "Hello, " + res.login + "! You are logged in! :)";
+      return;
+    }
+
+    oktaSignIn.renderEl(
+      { el: '#okta-login-container' },
+      function success(res) {},
+      function error(err) {
+        console.error(err);
+      }
+    );
+  });
+}
+
+function logout() {
+  console.log("signing out...")
+  oktaSignIn.signOut();
+  oktaSignIn.show();
+  location.reload()
+}
+
+function info() {
+  jQuery.ajax({
+      type: "POST",
+      url: '../php/scripts.php',
+      dataType: 'json',
+      data: {functionname: 'add', arguments: [1, 2]},
+
+      success: function (obj, textstatus) {
+                    if( !('error' in obj) ) {
+                        yourVariable = obj.result;
+                        console.log(yourVariable);
+                    }
+                    else {
+                        console.log(obj.error);
+                    }
+              }
+  });
+}
