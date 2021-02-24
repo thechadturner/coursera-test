0a8ce056d5be0b2cfc03720c802dc56d56cc9557
chad_turner <guyt2000@hotmail.com>
2021-02-25 10:52:26 +1300

    removed redirect function, cleaned up other parts of the code

--- Okta/Index/js/login.js
+++ Okta/Index/js/login.js
@@ -12,12 +12,12 @@ var oktaSignIn = new OktaSignIn({
 
 if (oktaSignIn.token.hasTokensInUrl()) {
   oktaSignIn.token.parseTokensFromUrl(
+  	console.log("recent sign in...") 
 
     // If we get here, the user just logged in.
     function success(res) {
-    	console.log("recent sign in..." + res) 
-    	console.log(res)
-		redirect(res)
+    	console.log("success! redirecting...") 
+		window.location = redirectUrl;
     },
     function error(err) {
       console.error(err);
@@ -30,31 +30,22 @@ else
   	console.log("already signed in..." + res.status) 
 
 	// If we get here, the user is already signed in.
-  	if (redirect(res) === false) {
-  		oktaSignIn.renderEl(
+  	if (res.status === 'SUCCESS' || res.status === 'ACTIVE') {
+    	console.log("redirecting...") 
+		window.location = redirectUrl;
+    }
+    else 
+    {
+    	oktaSignIn.renderEl(
 	      { el: '#okta-login-container' },
 	      function success(res) {
-			redirect(res)
 	      },
 	      function error(err) {
 	        console.error(err);
 	      }
 	    );
-  	}    
+    }  
   });
-}
-
-function redirect(res) {
-	if (res.status === 'SUCCESS' || res.status === 'ACTIVE') {
-    	console.log("redirecting...") 
-		window.location = redirectUrl;
-
-		return true
-    }
-    else
-    {
-    	return false
-    }
 }
 
 function logout() {
