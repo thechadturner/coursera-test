9a839d0e41d31389600d2a18194f97d4ab32bbd1
chad_turner <guyt2000@hotmail.com>
2021-02-26 10:30:38 +1300

    Changed groups to buttons instead of hrefs...

--- Okta/Index/js/user.js
+++ Okta/Index/js/user.js
@@ -11,7 +11,7 @@ oktaSignIn.session.get(function (res) {
 				let element = document.getElementById("projects")
 				let html = ""
 				groups.forEach(group => {
-					html += "<a href='#' onclick='selectProject("''+group+''")'><h1>"+group+"</h1></a>"
+					html += "<button onclick="myFunction(group)">"+group+"</button>"
 				});	
 				element.innerHTML = html	
 	  	});
