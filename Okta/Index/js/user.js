d960c89b0cba2fff41ca5f9e0f00a8081b576b89
chad_turner <guyt2000@hotmail.com>
2021-02-26 09:19:48 +1300

    Fixed missing bracket

--- Okta/Index/js/user.js
+++ Okta/Index/js/user.js
@@ -2,13 +2,13 @@ oktaSignIn.session.get(function (res) {
 	// If we get here, the user is already signed in.
 	if (res.status === 'ACTIVE') {
 	  getUserInfo().done(function(useroutput){
-	  	let userInfo = parseUserInfo(useroutput)
-	  	let groups = getGroupInfo(userInfo.id)
+	  	let userInfo = parseUserInfo(useroutput);
+	  	let groups = getGroupInfo(userInfo.id);
 
 	  	getGroupInfo(userInfo.id).done(function(groupoutput){
 				let groups = parseGroupInfo(groupoutput);
-				console.log(groups)
-	  	}
+				console.log(groups);
+	  	});
 
 	  	if (window.location.href.indexOf('user.html') > 0) {
 	  		document.getElementById("back").innerHTML = "<a href='#' onclick='logout()'><h3>Logout</h3></a>";
