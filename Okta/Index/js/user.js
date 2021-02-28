747488b72a9ddb89c4769b65d70aa66b87495cf4
chad_turner <guyt2000@hotmail.com>
2021-02-28 14:38:45 +1300

    Simplified user authentication

--- Okta/Index/js/user.js
+++ Okta/Index/js/user.js
@@ -1,30 +1,17 @@
 oktaSignIn.session.get(function (res) {
 	// If we get here, the user is already signed in.
-	if (res.status === 'ACTIVE') {	
-	    getUserInfo().done(function(useroutput){
-		  	let userInfo = parseUserInfo(useroutput);
-		  	sessionStorage.setItem("userInfo", JSON.stringify(userInfo.toString))
+	if (res.status === 'ACTIVE') {
+		let groups = JSON.parse(sessionStorage.getItem("projects"));
 
-		  	getGroupInfo(userInfo.id).done(function(groupoutput){
-				let groups = parseGroupInfo(groupoutput);
-				sessionStorage.setItem("projects", JSON.stringify(groups))
-
-				let element = document.getElementById("projects")
-				let html = ""
-				let index = 0
-				groups.forEach(group => {
-					console.log(group.description)
-					html += "<button onclick=selectProject("+index+")>"+group.description+"</button>"
-					index += 1
-				});	
-				element.innerHTML = html	
-		  	});
-
-		  	if (window.location.href.indexOf('user.html') > 0) {
-		  		document.getElementById("back").innerHTML = "<a href='#' onclick='logout()'><h3>Logout</h3></a>";
-		  		document.getElementById("top").innerHTML = "Welcome Back!";
-		  	}
-		});
+		let element = document.getElementById("projects")
+		let html = ""
+		let index = 0
+		groups.forEach(group => {
+			console.log(group.description)
+			html += "<button onclick=selectProject("+index+")>"+group.description+"</button>"
+			index += 1
+		});	
+		element.innerHTML = html
 
 	  	return;
 	} 
