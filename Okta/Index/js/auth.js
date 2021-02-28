db979e442c87d4d3082e98c98288204895370917
chad_turner <guyt2000@hotmail.com>
2021-02-28 15:41:16 +1300

    updated project auth.js

--- Okta/Index/js/auth.js
+++ Okta/Index/js/auth.js
@@ -1,3 +1,4 @@
+let allowaccess = false;
 let selectedProject = JSON.parse(localStorage.getItem("selectedProject"))
 
 if (window.location.href.indexOf('project.html') > 0) {
@@ -7,36 +8,29 @@ if (window.location.href.indexOf('project.html') > 0) {
 oktaSignIn.session.get(function (res) {
 	// If we get here, the user is signed in.
 	if (res.status === 'ACTIVE') {
-	    getUserInfo().done(function(useroutput){
-	    	let userInfo = parseUserInfo(useroutput);
-	  		sessionStorage.setItem("userInfo", JSON.stringify(userInfo))
+		let groups = JSON.parse(sessionStorage.getItem("projects"));
 
-		  	getGroupInfo(userInfo.id).done(function(groupoutput){
-				let groups = parseGroupInfo(groupoutput);
-				sessionStorage.setItem("projects", JSON.stringify(groups));
+		let html = ""
+		let index = 0
+		groups.forEach(group => {
+			if (group.name === selectedProject.name) {
+				allowaccess = true;
 
-				let allowaccess = false;
-				groups.forEach(group => {
-					if (group.name === selectedProject.name) {
-						allowaccess = true;
+				if (window.location.href.indexOf('project.html') > 0) {
+			  		if (selectedProject.name === undefined) {
+						window.location = 'https://thechadturner.github.io/coursera-test/Okta/Index/login.html'; 
+			  		}	
+			  	}
 
-						if (window.location.href.indexOf('project.html') > 0) {
-					  		if (selectedProject.name === undefined) {
-								window.location = 'https://thechadturner.github.io/coursera-test/Okta/Index/login.html'; 
-					  		}	
-					  	}
+				return;
+			}
+		});
 
-						return;
-					}
-				});
+		if (allowaccess === false) {
+			window.location = 'https://thechadturner.github.io/coursera-test/Okta/Index/denied.html'; 
+		}	
 
-				if (allowaccess === false) {
-					window.location = 'https://thechadturner.github.io/coursera-test/Okta/Index/denied.html'; 
-				}
-		  	});
-	    });
-
-	  return;
+	    return;
 	} 
 	else 
 	{
