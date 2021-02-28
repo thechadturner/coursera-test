467f4bb49d846462ae67b5c3f0cb3402d7118fce
chad_turner <guyt2000@hotmail.com>
2021-02-28 14:46:18 +1300

    Optimizing auth and login

--- Okta/Index/js/login.js
+++ Okta/Index/js/login.js
@@ -24,6 +24,19 @@ else
 
 	// If we get here, the user is already signed in.
   	if (res.status === 'ACTIVE') {
+  		getUserInfo().done(function(useroutput){
+	    	let userInfo = parseUserInfo(useroutput);
+	  		sessionStorage.setItem("userInfo", JSON.stringify(userInfo))
+
+	  		let labeltext = userInfo.firstName + "'s Projects"
+	  		document.getElementById("back").innerHTML = "<a href='Index/user.html'><h3>"+labeltext+"</h3></a>"; 
+
+		  	getGroupInfo(userInfo.id).done(function(groupoutput){
+				let groups = parseGroupInfo(groupoutput);
+				sessionStorage.setItem("projects", JSON.stringify(groups));
+		  	});
+	    });
+
 		window.location = redirectUrl;
 		return;
     }
--- Okta/Index/js/user.js
+++ Okta/Index/js/user.js
@@ -1,13 +1,12 @@
 oktaSignIn.session.get(function (res) {
 	// If we get here, the user is already signed in.
 	if (res.status === 'ACTIVE') {
+		let element = document.getElementById("projects")
 		let groups = JSON.parse(sessionStorage.getItem("projects"));
 
-		let element = document.getElementById("projects")
 		let html = ""
 		let index = 0
 		groups.forEach(group => {
-			console.log(group.description)
 			html += "<button onclick=selectProject("+index+")>"+group.description+"</button>"
 			index += 1
 		});	
